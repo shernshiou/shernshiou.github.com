b53493ebec1fb439a91ca97810edbca2f2b7f4dc
Shern Shiou <shernshiou@gmail.com>
2012-10-10 20:41:32 +0800

    Regenerate

--- config.rb
+++ config.rb
@@ -4,8 +4,8 @@ project_type = :stand_alone
 # Publishing paths
 http_path = "/"
 http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "fonts"
+css_dir = "/public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
